f8f561dd60f34b12f909716d6878f13f234e32ad
Cédric RENAUD <ruthhaba@gmail.com>
2019-10-14 10:22:08 +0200

    create files

---
+++ lib/0.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'pry'
+#-------Pour le scrapping-------
+#require 'nokogiri'
+#require 'open-uri'
+#-------Pour simuler un navigateur-------
+#require 'watir'
+#require 'launchy'
+#-------Pour les tests------
